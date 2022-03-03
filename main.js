67c003b702435859e21f0b5d9b2eb3b1db7e710d
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-03-03 00:31:09 -0300

    Finalizando o projeto AluraMidi, adicionando funcionalidades com uso pelo teclado

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSomPom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -11,7 +18,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
-        tocaSomPom(idAudio);
+        tocaSom(idAudio);
     }
     
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
